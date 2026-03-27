a8cdc22186d246143c8638c8044b44513b78343e
leoncharlescoaching <lookgoodfitnessltd@gmail.com>
2026-03-27 13:30:54 +0000

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,13 @@
 
+export const metadata = {
+  title: "Content Engine",
+  description: "Leon Charles Content Engine",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
